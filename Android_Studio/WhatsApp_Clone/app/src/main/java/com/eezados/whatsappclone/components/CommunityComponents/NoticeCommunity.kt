e864bf4d52b345e023773d9b760034e5bf5516fc
EliasZelaya <00146322@uca.edu.sv>
2024-04-23 19:21:03 -0600

    :memo: adding new proyect from class

--- Android_Studio/WhatsApp_Clone/app/src/main/java/com/eezados/whatsappclone/components/CommunityComponents/NoticeCommunity.kt
+++ Android_Studio/WhatsApp_Clone/app/src/main/java/com/eezados/whatsappclone/components/CommunityComponents/NoticeCommunity.kt
@@ -1,14 +1,54 @@
 package com.eezados.whatsappclone.components.CommunityComponents
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AllInbox
+import androidx.compose.material.icons.filled.BluetoothAudio
+import androidx.compose.material.icons.filled.Coronavirus
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Speaker
+import androidx.compose.material.icons.filled.SpeakerNotesOff
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NoticeCommunity() {
-    Column {
+    Row {
         ElevatedCard {
+            Row {
+                Icon(
+                    imageVector = Icons.Default.SpeakerNotesOff,
+                    contentDescription = "Icon Notice",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(
+                            Color.DarkGray
+                        )
+                )
+            }
+        }
 
+        Column {
+            Text("Notice")
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun Preview() {
+    NoticeCommunity()
 }
---
+++ Clases_PDM/Proyecto_Clase_2/app/src/main/java/com/eezados/lightbulb/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.eezados.lightbulb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eezados.lightbulb.ui.theme.LightbulbTheme
+import java.util.concurrent.Flow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LightbulbTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BulbLight() {
+    val checked = remember { mutableStateOf(true)}
+}
