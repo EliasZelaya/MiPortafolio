a10921f7470cd9154ee9e6b12ee51993760b3756
EliasZelaya <00146322@uca.edu.sv>
2024-04-25 23:27:27 -0600

    :memo: Create uca_life_v2

---
+++ Android_Studio/UCA_Life/app/src/main/java/com/elias/uca_life_v2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.elias.uca_life_v2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.elias.uca_life_v2.ui.theme.UCA_Life_V2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UCA_Life_V2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
