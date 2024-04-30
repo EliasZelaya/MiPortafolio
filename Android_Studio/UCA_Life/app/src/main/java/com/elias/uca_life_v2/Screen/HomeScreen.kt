fb789fb5e40cc5b1df15a5770e637e69f336e4c6
EliasZelaya <00146322@uca.edu.sv>
2024-04-30 17:16:37 -0600

    :memo: start with HomeScreen

---
+++ Android_Studio/UCA_Life/app/src/main/java/com/elias/uca_life_v2/Screen/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.elias.uca_life_v2.Screen
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    LazyColumn {
+        
+    }
+}
---
+++ Android_Studio/UCA_Life/app/src/main/java/com/elias/uca_life_v2/datalist/PostList.kt
@@ -0,0 +1,5 @@
+package com.elias.uca_life_v2.datalist
+
+data class PostList(
+    val ti: String,
+)
