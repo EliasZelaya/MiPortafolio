cee46378f10020e7fe3f90fa84b4533964955777
EliasZelaya <00146322@uca.edu.sv>
2024-05-01 18:44:59 -0600

    :memo: create textfield and make the logic in the viewmodel

--- Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/logincomponents/UsernameField.kt
+++ Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/logincomponents/UsernameField.kt
@@ -2,11 +2,24 @@ package com.elias.study_app.components.logincomponents
 
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import com.elias.study_app.viewmodel.LoginScreenViewModel
 
 @Composable
-fun UsernameField() {
+fun UsernameField(
+    modifier: Modifier,
+    viewmodel: LoginScreenViewModel
+) {
+    val username by viewmodel.username.observeAsState("")
+    val password by viewmodel.password.observeAsState("")
+
     TextField(
-        value = "",
-        onValueChange = {}
+        value = username,
+        onValueChange = {
+            viewmodel.onLoginField(it, password)
+        },
+        modifier = modifier
     )
 }
--- Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/screens/LoginScreen.kt
+++ Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/screens/LoginScreen.kt
@@ -6,14 +6,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.elias.study_app.components.logincomponents.UsernameField
+import com.elias.study_app.viewmodel.LoginScreenViewModel
 
 @Composable
 fun LoginScreen() {
+    val viewmodel: LoginScreenViewModel = viewModel()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        y
+        UsernameField(viewmodel)
     }
 }
