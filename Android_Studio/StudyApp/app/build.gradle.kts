aea9abab69d15c2677fd6a71cf82d34fbd639c19
EliasZelaya <00146322@uca.edu.sv>
2024-05-01 18:14:33 -0600

    Add: dependenci for livedata and viewmodel

--- Android_Studio/StudyApp/app/build.gradle.kts
+++ Android_Studio/StudyApp/app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     //SplashScreen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+    //viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
