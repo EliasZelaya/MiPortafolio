75234fce1576e2ff16e449b58c01eaaf8f71c09e
EliasZelaya <00146322@uca.edu.sv>
2024-05-02 23:55:44 -0600

    Add: Header Study Time and create labels for Subjects, Hours and goal hours

--- Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/homecomponents/InfoStudyTime.kt
+++ Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/homecomponents/InfoStudyTime.kt
@@ -1,23 +1,36 @@
 package com.elias.study_app.components.homecomponents
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun InfoStudyTime() {
-    OutlinedCard(modifier = Modifier
-        .height(100.dp)
-        .width(100.dp),
+    OutlinedCard(
+        modifier = Modifier
+            .height(75.dp)
+            .width(110.dp),
     ) {
-        Column {
-            Text("Subjects")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+//                .background(Color.Gray)
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Subjects", modifier = Modifier.padding(6.dp))
+            Text("10")
         }
     }
 }
--- Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/screens/HomeScreen.kt
+++ Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/screens/HomeScreen.kt
@@ -1,26 +1,53 @@
 package com.elias.study_app.screens
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.elias.study_app.components.homecomponents.InfoStudyTime
 
 @Composable
 fun HomeScreen() {
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
-            Text("Study Time")
+            Box( modifier = Modifier.fillMaxWidth().padding(12.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Study Time", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            }
+
+            Divider(modifier = Modifier.padding(bottom = 16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                InfoStudyTime()
+                InfoStudyTime()
+                InfoStudyTime()
+            }
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
