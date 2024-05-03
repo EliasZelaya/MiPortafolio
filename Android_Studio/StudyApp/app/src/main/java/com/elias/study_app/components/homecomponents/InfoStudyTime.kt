868dedfe819106313dac9d5d81c4538f605d9e51
EliasZelaya <00146322@uca.edu.sv>
2024-05-02 18:29:56 -0600

    Add: HomeScreen and remove navigation from viewmodel

--- Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/homecomponents/InfoStudyTime.kt
+++ Android_Studio/StudyApp/app/src/main/java/com/elias/study_app/components/homecomponents/InfoStudyTime.kt
@@ -1,2 +1,29 @@
 package com.elias.study_app.components.homecomponents
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InfoStudyTime() {
+    OutlinedCard(modifier = Modifier
+        .height(100.dp)
+        .width(100.dp),
+    ) {
+        Column {
+            Text("Subjects")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun InfoStudyPreview() {
+    InfoStudyTime()
+}
